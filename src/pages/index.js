f7d8f14a93f12b2a2203c94d83d38c3ad3d56fc8
Ryan Esmores <ryanesmores07@gmail.com>
2022-02-12 15:29:41 +0900

    worked with graphql for posts component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,40 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
   return (
     <Layout>
       <Hero showPerson />
+      <Posts posts={posts} title="recently published" />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allMdx(limit: 3, sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`
 
 export default IndexPage
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -3,12 +3,39 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
-const PostsPage = () => {
+const PostsPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
   return (
     <Layout>
       <Hero />
+      <Posts posts={posts} title="all posts" />
     </Layout>
   )
 }
 
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`
 export default PostsPage
