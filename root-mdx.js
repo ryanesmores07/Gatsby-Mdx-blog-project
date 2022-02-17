8b0bfebce5e6db7f30a8676508d74e359682a1a5
Ryan Esmores <ryanesmores07@gmail.com>
2022-02-17 22:32:56 +0900

    worked with root-mdx

---
+++ root-mdx.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+
+const components = {
+  h2: props => {
+    return <h2 {...props}>{props.children}</h2>
+  },
+}
+
+export const wrapMDX = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>
+}
